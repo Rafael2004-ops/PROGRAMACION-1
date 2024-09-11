eb45c1cd78e703ce9fdcd7e48b00392cad0b70b8
Rafael2004-ops <rafael_escalante@soy.sena.edu.co>
2024-09-11 18:01:15 -0500

    Add files via upload

---
+++ Ejercicio_en_clase11sept.py
@@ -0,0 +1,51 @@
+# https://www.programiz.com/python-programming/online-compiler/
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+
+import math
+
+#Es la variable del número uno
+numerouno = int(input("Digite el numero uno "))
+#Es la variable del número dos
+numerodos = int(input("Digite el numero dos "))
+
+#Es la variable del número tres
+numerotres = int(input("Digite el numero tres "))
+#Es la variable del número cuatro
+numerocuatro = int(input("Digite el numero cuatro "))
+
+#Es la variable del número cinco
+numerocinco = int(input("Digite el numero cinco "))
+
+
+#Es la variable operacion de suma de todos los numeros
+suma = numerouno + numerodos +numerotres + numerocuatro + numerocinco
+
+#Es la variable operacion resultado del producto de todos los números
+producto = numerouno * numerodos * numerotres * numerocuatro * numerocinco
+
+#Es la variable operacion promedio de todos los números
+promedio = (numerouno + numerodos +numerotres + numerocuatro + numerocinco)/5
+
+#Es la variable operacion area al cuadrado
+
+unocuadrado = numerouno**2
+print("cuadrado del numero uno", unocuadrado)
+doscuadrado = numerodos**2
+print("cuadrado del numero dos", doscuadrado)
+trescuadrado = numerotres**2
+print("cuadrado del numero tres", trescuadrado)
+cuatrocuadrado = numerocuatro**2
+print("cuadrado del numero cuatro", cuatrocuadrado)
+cincocuadrado = numerocinco**2
+print("cuadrado del numero cinco", cincocuadrado)
+
+
+#Impresion de resultados
+print(suma)
+print(producto)
+print(promedio)
+
+
+
+
