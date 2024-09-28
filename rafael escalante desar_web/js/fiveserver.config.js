6f15c8a44060b99fa75e5b6e00af071f547e8ebb
Rafael2004-ops <rafael_escalante@soy.sena.edu.co>
2024-09-28 16:40:07 -0500

    Add files via upload

---
+++ rafael escalante desar_web/js/fiveserver.config.js
@@ -0,0 +1,11 @@
+// fiveserver.config.js
+module.exports = {
+    Highlight: true, // enable highlight feature
+    injectbbody: true, // enable instant update
+    remotelogs: true, // enable remotelogs
+    remotelogs: "yellow", // enable reomelogs and use the color yellow
+    injectcss: true, // disable injecting css
+    navigate: true, //enable auto-navigation
+};
+
+    
