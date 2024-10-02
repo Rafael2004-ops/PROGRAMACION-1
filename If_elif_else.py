1365d5f72157ecb6063bc7447d7b9bbd7e8c19ee
Rafael2004-ops <rafael_escalante@soy.sena.edu.co>
2024-10-02 16:00:56 -0500

    Create If_elif_else.py

---
+++ If_elif_else.py
@@ -0,0 +1,27 @@
+#Reaizar un programa que me imgrese un numero entero 
+
+#si es positivo o si es negativo
+
+
+
+#datos de entradad
+
+numero = int(input("ingrese numero"))
+
+
+
+#procesamiento
+
+if numero > 0:
+
+    print("el numero es positivo")
+
+elif numero == 0:
+
+    print("el numero es neutro")
+
+else:
+
+    print("el numero es negativo")
+
+    
